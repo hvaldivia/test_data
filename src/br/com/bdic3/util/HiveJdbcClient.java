ddbb445a1cc6f955e499ba0c744a1caad39533f7
Rene Esteves <rene.stvs@gmail.com>
2014-05-15 20:29:19 -0700

    SPRINT2_Fase1_transacaoincomun

--- src/br/com/bdic3/util/HiveJdbcClient.java
+++ src/br/com/bdic3/util/HiveJdbcClient.java
@@ -6,6 +6,8 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.sql.DriverManager;
 
+import javax.swing.JOptionPane;
+
 public class HiveJdbcClient {
 	private static String driverName = "org.apache.hadoop.hive.jdbc.HiveDriver";
 
@@ -14,6 +16,12 @@ public class HiveJdbcClient {
 	 * @throws SQLException
 	 */
 	public static void main(String[] args) throws SQLException {
+		
+		//Captura Entrada
+		String cli_id = JOptionPane.showInputDialog("Qual é o seu nome?");
+		String valor = JOptionPane.showInputDialog("Qual é o valor da compra?");
+		
+		
 		try {
 			Class.forName(driverName);
 		} catch (ClassNotFoundException e) {
@@ -26,10 +34,27 @@ public class HiveJdbcClient {
 				"jdbc:hive://localhost:10000/default", "", "");
 		Statement stmt = con.createStatement();
 		stmt.execute("use bdic3");
-		ResultSet res = stmt.executeQuery("select * from localidade");
+		
+		float valor_max = 0;
+		ResultSet res = stmt
+				.executeQuery("select max(transacao.tra_total) from transacao where transacao.cli_id = "+ cli_id);
+		
 		while (res.next()) {
-			System.out.println(res.getString(1));
+			valor_max = res.getFloat(1);
+			System.out.println("valor_max = " + valor_max);
 		}
 
+		float first_quartile = valor_max / 4;
+		System.out.println("first_quartile = " + first_quartile);
+		
+		float third_quartile = first_quartile * 3;
+		System.out.println("third_quartile = " + third_quartile);
+		
+		float upper_limit = (float) (third_quartile + 1.5 * (third_quartile - first_quartile));
+		System.out.println("upper_limit = " + upper_limit);
+		
+		if (Float.parseFloat(valor) >  upper_limit){
+			JOptionPane.showConfirmDialog(null, "ATENÇÃO TRANSAÇÃO INCOMUN", " Você realizou esta compra " + cli_id + "?", 0);
+		}
 	}
 }
